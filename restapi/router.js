047641316b1b93ad509faed1fc04683436d2047f
Francisco Naranjo <fnaranjo@fnaranjo.ibm.com>
2017-05-03 16:28:36 -0500

    Second Function

--- restapi/router.js
+++ restapi/router.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var spot = require("./spot/readSpotData");
+var spot2 = require("./spot/readSpotData2");
 module.exports=router;
 
 router.get("/spotData", spot.getData);
+router.get("/spotData2", spot2.getData2);
